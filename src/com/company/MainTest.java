7ae376a4639e85bdbf626aec2d931fd30512d282
karthikr <karthikr@INkarthikr.local>
2015-08-25 14:21:39 +0530

    Commit 1

---
+++ src/com/company/MainTest.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import junit.framework.TestCase;
+import org.junit.Assert.*;
+
+/**
+ * Created by karthikr on 25/08/15.
+ */
+public class MainTest extends TestCase {
+
+    public static void main(String[] args){
+        assertEquals(1,Main.getlength());
+
+
+}
